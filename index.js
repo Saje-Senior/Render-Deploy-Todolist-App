ee3ce35cf5a102ae20f73572cf0f4a1322f0a1e3
Saje Senior <saje01@live.com>
2025-03-09 15:51:17 -0400

    Update

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
-const path = require("path");
+// GET, POST, PUT, DELETE, PATCH
+const path = require("path"); 
 const express = require('express');
+
 const uuid = require('uuid');
+const app = express();
 const cors = require('cors');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// app.use - represent the incoming HTTP request that consists of data like parameters, query strings, and also the request body
+// automatically parse incoming request bodies as JSON. When a request is made to the server with a JSON payload, this middleware will parse it and make the parsed data available in req.body
 
 app.use(express.json());
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
+const PORT = process.env.PORT || 5000;
 
-const todos = [];
+const todos = [
+]
+
+// Serve static files (CSS, JS, images) from the "public" directory
+app.use(express.static(path.join(__dirname, ".")));
+
+// Serve index.html
+
+// app.get - route HTTP GET requests to the specified path, associating them with designated callback functions.
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+    res.json({
+        msg: "Todo List Home Page"
+    })
+})
 
-app.get('/api/home', (req, res) => {
-    res.json({ msg: "Todo List Home Page" });
-});
+app.get('/todos', (req, res) => {
+    res.json(todos)
+})
 
-app.get('/api/todos', (req, res) => {
+// req.params.id - used in Express.js to access route parameters from a URL. 
+// It retrieves the value of the id parameter when defined in a route.
+// Use Case:
+// This is useful for retrieving dynamic values from URLs, such as:
+
+// Fetching a specific user's details (/users/:id)
+// Editing a specific post (/posts/:postId/edit)
+// Deleting an item (/products/:productId/delete)
+
+app.get('/todos/:id', (req, res) => {
+    let todo = todos.filter((todo) => todo.id == req.params.id )
+    res.json({ msg: `Todo ${req.params.id}`, data: todo })
+})
+
+// app.post - sending some data along with the request to the HTTP server.
+
+app.post('/todos', (req, res) => {
+    todos.push({ id: uuid.v4(), ...req.body })
+    res.json({ msg: "Add Todo", data: todos })
+})
+
+// app.put - used to send data to a server to create/update a resource.
+
+app.put('/todos/:id', (req, res) => {
+    let todo = todos.find((todo) => todo.id == req.params.id)
+    if(todo) {
+        todo.name = req.body.name
+        todo.completed = req.body.completed
+        res.json({ msg: "Edit/Update Todo", data: todos })
+    }
+    else{
+        res.json({ msg: "Todo not found" })
+    }
+})
+
+// app.delete - handle HTTP DELETE requests to a specified path.
+
+
+app.delete("/todos/:id", (req, res) => {
+    let index = todos.findIndex((todo) => todo.id == req.params.id);
+    todos.splice(index, 1);
     res.json(todos);
 });
 
-app.get('/api/todos/:id', (req, res) => {
-    let todo = todos.filter((todo) => todo.id == req.params.id);
-    res.json({ msg: `Todo ${req.params.id}`, data: todo });
-});
-
-app.post('/api/todos', (req, res) => {
-    todos.push({ id: uuid.v4(), ...req.body });
-    res.json({ msg: "Add Todo", data: todos });
-});
-
-app.put('/api/todos/:id', (req, res) => {
-    let todo = todos.find((todo) => todo.id == req.params.id);
-    if (todo) {
-        todo.name = req.body.name;
-        todo.completed = req.body.completed;
-        res.json({ msg: "Edit/Update Todo", data: todos });
-    } else {
-        res.status(404).json({ msg: "Todo not found" });
-    }
-});
-
-app.delete("/api/todos/:id", (req, res) => {
-    let index = todos.findIndex((todo) => todo.id == req.params.id);
-    if (index !== -1) {
-        todos.splice(index, 1);
-        res.json(todos);
-    } else {
-        res.status(404).json({ msg: "Todo not found" });
-    }
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
 app.listen(PORT, () => {
-    console.log(`Server Started at ${PORT}`);
-});
+    console.log(`Server Started at ${PORT}`)
+})
+
+// app.patch - used to update specific fields of a resource, unlike PUT, which replaces the entire resource.
